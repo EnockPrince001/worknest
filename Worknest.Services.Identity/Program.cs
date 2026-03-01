d1acd1ffce3c4cdba6c1efa90ff8c0a328c830a2
Enock Borabu <enockprince001@gmail.com>
2026-03-01 13:08:41 +0300

    Add auto-migration to Identity service

--- Worknest.Services.Identity/Program.cs
+++ Worknest.Services.Identity/Program.cs
@@ -84,6 +84,20 @@ app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthentication(); 
 app.UseAuthorization(); 
 
-app.MapControllers();
+// --- 9. Auto-migrate database on startup ---
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
 
 app.Run();
